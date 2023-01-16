b43fd813e9d7618e37964a099ad39cfd6fbf630d
chenkai <chenkai@soulapp.cn>
2023-01-16 18:47:43 +0800

    macos_gl_version_4.1

--- examples/benchmark-opengl.c
+++ examples/benchmark-opengl.c
@@ -121,7 +121,7 @@ int main(void)
         exit(EXIT_FAILURE);
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #if defined(__APPLE__)
